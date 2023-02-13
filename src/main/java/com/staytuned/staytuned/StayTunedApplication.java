e303c32f229de9b8b7acdd8b396c71c35b7afac9
jueun275 <52851721+jueun275@users.noreply.github.com>
2023-02-13 13:00:42 +0900

    Initial commit

---
+++ src/main/java/com/staytuned/staytuned/StayTunedApplication.java
@@ -0,0 +1,11 @@
+package com.staytuned.staytuned;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StayTunedApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StayTunedApplication.class, args);
+    }
+}
