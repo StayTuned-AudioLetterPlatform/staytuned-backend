e26af79a8ff81f4d4a712ee7c0b9635beabe9e66
jueun275 <52851721+jueun275@users.noreply.github.com>
2023-03-16 02:25:30 +0900

    .yml에서 값 가져오기

--- src/main/java/com/staytuned/staytuned/StayTunedApplication.java
+++ src/main/java/com/staytuned/staytuned/StayTunedApplication.java
@@ -1,9 +1,12 @@
 package com.staytuned.staytuned;
 
+import com.staytuned.staytuned.security.config.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AppProperties.class)
 public class StayTunedApplication {
     public static void main(String[] args) {
         SpringApplication.run(StayTunedApplication.class, args);
---
+++ src/main/java/com/staytuned/staytuned/security/config/AppProperties.java
@@ -0,0 +1,20 @@
+package com.staytuned.staytuned.security.config;
+
+import lombok.NoArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+
+@NoArgsConstructor
+@ConfigurationProperties(prefix = "app.oauth2")
+public class AppProperties {
+    private String authorizedRedirectUri;
+
+    public String getAuthorizedRedirectUri() {
+        return authorizedRedirectUri;
+    }
+
+    public void setAuthorizedRedirectUri(String authorizedRedirectUri) {
+        this.authorizedRedirectUri = authorizedRedirectUri;
+    }
+}
